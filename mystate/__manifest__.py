e8a79b7e7cd5c3e30a8461b50d6d9380a8fbcd3e
igeodoo <ige@odoo.com>
2019-03-22 16:09:23 +0100

    myimport
    le bon cette fois

---
+++ mystate/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'France - States',
+    'version': '1.0',
+    'category': 'Accounting',
+    'description': """
+Importing France States
+""",
+    'depends': ['crm'],
+    'data': [
+        'data/res.country.state.csv',
+        
+    ],
+   
